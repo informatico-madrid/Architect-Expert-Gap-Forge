34ae02681da5e5186a130ca256d3eea7ea6fbdbd
Joao Maria Arranz Aparicio <joao@informatico-madrid.com>
2026-03-09 19:32:57 +0000

    add mising headers

--- tests/test_sanitize_regression.py
+++ tests/test_sanitize_regression.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# Architect-Expert-Gap-Forge (AEGF)
+# Copyright (c) 2026 Joao Maria Arranz Aparicio <joao@informatico-madrid.com>
+# SPDX-License-Identifier: Apache-2.0
+
 """
 Regression tests for _sanitize_generated_reasoning() corruption issue.
 
